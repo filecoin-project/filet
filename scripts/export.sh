a7d3f966015216c11fc02c052122af4ac480c3c9
kasteph <hello@stephsamson.com>
2023-01-30 20:43:16 +0800

    chore: tee to lily.log (#28)

--- scripts/export.sh
+++ scripts/export.sh
@@ -35,7 +35,7 @@ echo "Running daemon..."
 lily daemon \
     --repo="${REPO_PATH}" \
     --config=/lily/config.toml \
-    --bootstrap=false &> lily.log &
+    --bootstrap=false &> >(tee lily.log) &
 
 # Wait for Lily to come online
 lily wait-api
